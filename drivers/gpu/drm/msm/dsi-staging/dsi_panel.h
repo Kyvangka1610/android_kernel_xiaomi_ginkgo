5d91cb5d8075bc703ec4fa374ed7555424217166
Danny Baumann <dannybaumann@web.de>
2018-07-19 09:41:23 +0200

    drm: msm: dsi-staging: Add support for High Brightness Mode
    Author: Arian <arian.kulmer@web.de>
    Date:   Wed Jun 24 15:44:22 2020 +0200
    
        drm: msm: Use type_map array index 0 for invalid hbm values
    
        Change-Id: I8b29ad7227be9f01f945af136747a5924b0b628a
    
    [ghostrider-reborn] Adapted to ginkgo LCD HBM
    Change-Id: Id133e30c7bf645386053c42d4d121a053b03bac7
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -217,6 +217,8 @@ struct dsi_panel {
 	bool sync_broadcast_en;
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
+
+	int hbm_mode;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -341,4 +343,7 @@ void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
 
 void dsi_panel_calc_dsi_transfer_time(struct dsi_host_common_cfg *config,
 		struct dsi_display_mode *mode, u32 frame_threshold_us);
+
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
+
 #endif /* _DSI_PANEL_H_ */
