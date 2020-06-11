eb7a000a4ff8b45dd26214db00ddf809e2763184
Kyvangka1610 <kyvangka2002@gmail.com>
2020-06-11 13:45:12 +0700

    ADD Setup To Build
    Signed-off-by: Kyvangka1610 <kyvangka2002@gmail.com>

---
+++ build.sh
@@ -0,0 +1,29 @@
+export KBUILD_BUILD_USER="elang"
+export KBUILD_BUILD_HOST="kyvangkaelang"
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_COMPILER_STRING=$(/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+LD_LIBRARY_PATH=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/lib:/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+PATH=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/:$PATH
+export PATH
+export PROCS=$(nproc --all)
+export CROSS_COMPILE=/home/kyvangka1610/kernel/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/home/kyvangka1610/kernel/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CC=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/clang
+export AR=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-ar
+export NM=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-nm
+export OBJCOPY=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-objcopy
+export OBJDUMP=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-objdump
+export STRIP=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-strip
+make O=out vendor/ginkgo-perf_defconfig
+make -j$PROCS O=out ARCH=arm64 \
+	CROSS_COMPILE=$CROSS_COMPILE \
+	CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 \
+	CC=$CC \
+	AR=$AR \
+	NM=$NM \
+	OBJCOPY=$OBJCOPY \
+	OBJDUMP=$OBJDUMP \
+	STRIP=$STRIP \
+	CLANG_TRIPLE=aarch64-linux-gnu-
