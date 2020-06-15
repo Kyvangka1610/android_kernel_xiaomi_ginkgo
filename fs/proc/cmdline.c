753aeb379969d29b6780b419ccfbb59d2a784dcf
Kyvangka1610 <kyvangka2002@gmail.com>
2020-06-15 13:45:08 +0700

    Remove Shit Merging
    Signed-off-by: Kyvangka1610 <kyvangka2002@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -9,12 +9,8 @@ static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-<<<<<<< HEAD
-	seq_printf(m, "%s\n", saved_command_line);
-=======
 	seq_puts(m, new_command_line);
 	seq_putc(m, '\n');
->>>>>>> dd3d9c721717... proc: cmdline: Patch SafetyNet flags
 	return 0;
 }
 
