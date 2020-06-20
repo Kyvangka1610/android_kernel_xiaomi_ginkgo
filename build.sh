26bfec13e38f3c1cc474ddc3af5891539d9e4e91
Kyvangka1610 <kyvangka2002@gmail.com>
2020-06-20 16:06:35 +0700

    Update kernel build.sh
    Signed-off-by: Kyvangka1610 <kyvangka2002@gmail.com>

--- build.sh
+++ build.sh
@@ -2,20 +2,20 @@ export KBUILD_BUILD_USER="elang"
 export KBUILD_BUILD_HOST="kyvangkaelang"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING=$(/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-LD_LIBRARY_PATH=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/lib:/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/lib64:$LD_LIBRARY_PATH
+export KBUILD_COMPILER_STRING=$(/home/kyvangka1610/kernel/clang-r383902c/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+LD_LIBRARY_PATH=/home/kyvangka1610/kernel/clang-r383902c/lib:/home/kyvangka1610/kernel/clang-r383902c/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
-PATH=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/:$PATH
+PATH=/home/kyvangka1610/kernel/clang-r383902c/bin/:$PATH
 export PATH
 export PROCS=$(nproc --all)
 export CROSS_COMPILE=/home/kyvangka1610/kernel/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=/home/kyvangka1610/kernel/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-export CC=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/clang
-export AR=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-ar
-export NM=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-nm
-export OBJCOPY=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-objcopy
-export OBJDUMP=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-objdump
-export STRIP=/home/kyvangka1610/kernel/android_prebuilts_clang_host_linux-x86_clang-6284175/bin/llvm-strip
+export CC=/home/kyvangka1610/kernel/clang-r383902c/bin/clang
+export AR=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-ar
+export NM=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-nm
+export OBJCOPY=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-objcopy
+export OBJDUMP=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-objdump
+export STRIP=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-strip
 export out=/home/kyvangka1610/out
 make O=$out vendor/ginkgo-perf_defconfig
 make -j$PROCS O=$out ARCH=arm64 \
