5bb3ee45b7d47411a8575c2edcd7b25ad4867d29
Kyvangka1610 <kyvangka2002@gmail.com>
2020-08-12 14:40:56 +0700

    Kerne Update : build.sh Use Proton Clang
    Signed-off-by: Kyvangka1610 <kyvangka2002@gmail.com>

--- build.sh
+++ build.sh
@@ -2,20 +2,20 @@ export KBUILD_BUILD_USER="elang"
 export KBUILD_BUILD_HOST="kyvangkaelang"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING=$(/home/kyvangka1610/kernel/clang-r383902c/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-LD_LIBRARY_PATH=/home/kyvangka1610/kernel/clang-r383902c/lib:/home/kyvangka1610/kernel/clang-r383902c/lib64:$LD_LIBRARY_PATH
+export KBUILD_COMPILER_STRING=$(/home/kyvangka1610/toolchain/clang11/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+LD_LIBRARY_PATH=/home/kyvangka1610/toolchain/clang11/lib:/home/kyvangka1610/toolchain/clang11/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
-PATH=/home/kyvangka1610/kernel/clang-r383902c/bin/:$PATH
+PATH=/home/kyvangka1610/toolchain/clang11/bin/:$PATH
 export PATH
 export PROCS=$(nproc --all)
 export CROSS_COMPILE=/home/kyvangka1610/kernel/aarch64-maestro-linux-gnu/bin/aarch64-maestro-linux-gnu-
 export CROSS_COMPILE_ARM32=/home/kyvangka1610/kernel/arm-maestro-linux-gnueabi/bin/arm-maestro-linux-gnueabi-
-export CC=/home/kyvangka1610/kernel/clang-r383902c/bin/clang
-export AR=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-ar
-export NM=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-nm
-export OBJCOPY=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-objcopy
-export OBJDUMP=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-objdump
-export STRIP=/home/kyvangka1610/kernel/clang-r383902c/bin/llvm-strip
+export CC=/home/kyvangka1610/toolchain/clang11/bin/clang
+export AR=/home/kyvangka1610/toolchain/clang11/bin/llvm-ar
+export NM=/home/kyvangka1610/toolchain/clang11/bin/llvm-nm
+export OBJCOPY=/home/kyvangka1610/toolchain/clang11/bin/llvm-objcopy
+export OBJDUMP=/home/kyvangka1610/toolchain/clang11/bin/llvm-objdump
+export STRIP=/home/kyvangka1610/toolchain/clang11/bin/llvm-strip
 export out=/home/kyvangka1610/out
 make O=$out vendor/ginkgo-perf_defconfig
 make -j$PROCS O=$out ARCH=arm64 \
