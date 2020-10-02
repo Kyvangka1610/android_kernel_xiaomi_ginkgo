9ebe6e5f319096c36ba48d40c43d89067e6fda74
kyvangka1610 <kyvangka2002@gmail.com>
2020-10-02 10:18:56 +0700

    SixTeen : Update build.sh
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- build.sh
+++ build.sh
@@ -2,21 +2,21 @@ export KBUILD_BUILD_USER="elang"
 export KBUILD_BUILD_HOST="kyvangkaelang"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING=$(/home/kyvangka1610/toolchain/clang11/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-LD_LIBRARY_PATH=/home/kyvangka1610/toolchain/clang11/lib:/home/kyvangka1610/toolchain/clang11/lib64:$LD_LIBRARY_PATH
+export KBUILD_COMPILER_STRING=$(/home/kyvangka1610/toolchain/Clang12/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+LD_LIBRARY_PATH=/home/kyvangka1610/toolchain/Clang12/lib:/home/kyvangka1610/toolchain/Clang12/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
-PATH=/home/kyvangka1610/toolchain/clang11/bin/:$PATH
+PATH=/home/kyvangka1610/toolchain/Clang12/bin/:$PATH
 export PATH
 export PROCS=$(nproc --all)
-export CROSS_COMPILE=/home/kyvangka1610/kernel/aarch64-maestro-linux-gnu/bin/aarch64-maestro-linux-gnu-
-export CROSS_COMPILE_ARM32=/home/kyvangka1610/kernel/arm-maestro-linux-gnueabi/bin/arm-maestro-linux-gnueabi-
-export CC=/home/kyvangka1610/toolchain/clang11/bin/clang
-export AR=/home/kyvangka1610/toolchain/clang11/bin/llvm-ar
-export NM=/home/kyvangka1610/toolchain/clang11/bin/llvm-nm
-export LD=/home/kyvangka1610/toolchain/clang11/bin/ld.lld
-export OBJCOPY=/home/kyvangka1610/toolchain/clang11/bin/llvm-objcopy
-export OBJDUMP=/home/kyvangka1610/toolchain/clang11/bin/llvm-objdump
-export STRIP=/home/kyvangka1610/toolchain/clang11/bin/llvm-strip
+export CROSS_COMPILE=/home/kyvangka1610/toolchain/aarch64-maestro-linux-gnu/bin/aarch64-maestro-linux-gnu-
+export CROSS_COMPILE_ARM32=/home/kyvangka1610/toolchain/arm-maestro-linux-gnueabi/bin/arm-maestro-linux-gnueabi-
+export CC=/home/kyvangka1610/toolchain/Clang12/bin/clang
+export AR=/home/kyvangka1610/toolchain/Clang12/bin/llvm-ar
+export NM=/home/kyvangka1610/toolchain/Clang12/bin/llvm-nm
+export LD=/home/kyvangka1610/toolchain/Clang12/bin/ld.lld
+export OBJCOPY=/home/kyvangka1610/toolchain/Clang12/bin/llvm-objcopy
+export OBJDUMP=/home/kyvangka1610/toolchain/Clang12/bin/llvm-objdump
+export STRIP=/home/kyvangka1610/toolchain/Clang12/bin/llvm-strip
 export out=/home/kyvangka1610/out-new
 make O=$out vendor/sixteen_defconfig
 make -j$PROCS O=$out ARCH=arm64 \
