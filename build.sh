1822ae3929355c534bc299f06eee82a7dda3fa70
kyvangka1610 <kyvangka2002@gmail.com>
2020-10-09 08:16:09 +0700

    sixteen : update build.sh
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- build.sh
+++ build.sh
@@ -1,30 +1,34 @@
+# ENV
+CONFIG=vendor/sixteen_defconfig
+KERNEL_DIR=$(pwd)
+PARENT_DIR="$(dirname "$KERNEL_DIR")"
+KERN_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
 export KBUILD_BUILD_USER="elang"
 export KBUILD_BUILD_HOST="kyvangkaelang"
-export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_COMPILER_STRING=$(/home/kyvangka1610/toolchain/clang-r377782d/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-LD_LIBRARY_PATH=/home/kyvangka1610/toolchain/clang-r377782d/lib:/home/kyvangka1610/toolchain/clang-r377782d/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH
-PATH=/home/kyvangka1610/toolchain/clang-r377782d/bin/:$PATH
-export PATH
-export PROCS=$(nproc --all)
+export PATH="/home/kyvangka1610/toolchain/Clang12/bin:$PATH"
+export LD_LIBRARY_PATH="/home/kyvangka1610/toolchain/Clang12/lib:$LD_LIBRARY_PATH"
+export KBUILD_COMPILER_STRING="$(/home/kyvangka1610/toolchain/Clang12/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
 export CROSS_COMPILE=/home/kyvangka1610/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=/home/kyvangka1610/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-export CC=/home/kyvangka1610/toolchain/clang-r377782d/bin/clang
-export AR=/home/kyvangka1610/toolchain/clang-r377782d/bin/llvm-ar
-export NM=/home/kyvangka1610/toolchain/clang-r377782d/bin/llvm-nm
-export OBJCOPY=/home/kyvangka1610/toolchain/clang-r377782d/bin/llvm-objcopy
-export OBJDUMP=/home/kyvangka1610/toolchain/clang-r377782d/bin/llvm-objdump
-export STRIP=/home/kyvangka1610/toolchain/clang-r377782d/bin/llvm-strip
 export out=/home/kyvangka1610/out-new-R
-make O=$out vendor/sixteen_defconfig
-make -j$PROCS O=$out ARCH=arm64 \
-	CROSS_COMPILE=$CROSS_COMPILE \
-	CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 \
-	CC=$CC \
-	NM=$NM \
-	LD=$LD \
-	OBJCOPY=$OBJCOPY \
-	OBJDUMP=$OBJDUMP \
-	STRIP=$STRIP \
-	CLANG_TRIPLE=aarch64-linux-gnu-
+# Functions
+clang_build () {
+    make -j4 O=$out \
+                          ARCH=arm64 \
+                          CC="clang" \
+                          AR="llvm-ar" \
+                          NM="llvm-nm" \
+					      LD="ld.lld" \
+			              AS="llvm-as" \
+			              OBJCOPY="llvm-objcopy" \
+			              OBJDUMP="llvm-objdump" \
+                          CLANG_TRIPLE=aarch64-linux-gnu- \
+                          CROSS_COMPILE=$CROSS_COMPILE \
+                          CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32
+}
+
+# Build kernel
+make O=$out ARCH=arm64 $CONFIG > /dev/null
+echo -e "${bold}Compiling with CLANG${normal}\n$KBUILD_COMPILER_STRING"
+clang_build
+
