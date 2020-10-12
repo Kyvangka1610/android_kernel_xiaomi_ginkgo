36d3784520fd92b37a576390ea4fa52cfd8ded78
kyvangka1610 <kyvangka2002@gmail.com>
2020-10-12 14:26:42 +0700

    build.sh : add time build
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+echo -e "\nStarting compilation...\n"
 # ENV
 CONFIG=vendor/sixteen_defconfig
 KERNEL_DIR=$(pwd)
@@ -11,6 +12,7 @@ export KBUILD_COMPILER_STRING="$(/home/kyvangka1610/toolchain/Clang12/bin/clang 
 export CROSS_COMPILE=/home/kyvangka1610/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=/home/kyvangka1610/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 export out=/home/kyvangka1610/out-new-R
+
 # Functions
 clang_build () {
     make -j4 O=$out \
@@ -18,10 +20,10 @@ clang_build () {
                           CC="clang" \
                           AR="llvm-ar" \
                           NM="llvm-nm" \
-					      LD="ld.lld" \
-			              AS="llvm-as" \
-			              OBJCOPY="llvm-objcopy" \
-			              OBJDUMP="llvm-objdump" \
+			   LD="ld.lld" \
+			   AS="llvm-as" \
+			   OBJCOPY="llvm-objcopy" \
+			   OBJDUMP="llvm-objdump" \
                           CLANG_TRIPLE=aarch64-linux-gnu- \
                           CROSS_COMPILE=$CROSS_COMPILE \
                           CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32
@@ -31,4 +33,4 @@ clang_build () {
 make O=$out ARCH=arm64 $CONFIG > /dev/null
 echo -e "${bold}Compiling with CLANG${normal}\n$KBUILD_COMPILER_STRING"
 clang_build
-
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
