ce609925de98c679ea2dc48d949f40b264c654df
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-04 15:39:47 +0530

    kernel/cgroup: Boost DDR in addition to CPU on app launch
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -15,6 +15,7 @@
 #include <linux/cgroupstats.h>
 #include <linux/binfmts.h>
 #include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 #include <trace/events/cgroup.h>
 
@@ -558,6 +559,7 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
 		task_is_zygote(current)) {
 		cpu_input_boost_kick_max(1000);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
 	}
 
 out_finish:
