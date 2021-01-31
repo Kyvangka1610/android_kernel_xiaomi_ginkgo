e7d96ce11a7e35f91c3f4156c1aecc798fa55fe1
kyvangka1610 <kyvangka2002@gmail.com>
2021-01-31 10:26:56 +0700

    SixTeen : build.sh regen
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo -e "\nStarting compilation...\n"
 CONFIG=vendor/sixteen_defconfig
 KERNEL_DIR=$(pwd)
 PARENT_DIR="$(dirname "$KERNEL_DIR")"
-KERN_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
+KERN_IMG="/home/kyvangka1610/out-new-Q/out/arch/arm64/boot/Image.gz-dtb"
 export KBUILD_BUILD_USER="elang"
 export KBUILD_BUILD_HOST="kyvangkaelang"
 export PATH="/home/kyvangka1610/toolchain/clang-r407598b/bin:$PATH"
@@ -30,5 +30,27 @@ clang_build () {
 # Build kernel
 make O=$out ARCH=arm64 $CONFIG > /dev/null
 echo -e "${bold}Compiling with CLANG${normal}\n$KBUILD_COMPILER_STRING"
+echo -e "\nCompiling $ZIPNAME\n"
 clang_build
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+if [ -f "$out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "$out/arch/arm64/boot/dtbo.img" ]; then
+ echo -e "\nKernel compiled succesfully! Zipping up...\n"
+ ZIPNAME="SixTeen•Kernel•Q•Ginkgo•Willow-$(date '+%Y%m%d-%H%M').zip"
+ if [ ! -d AnyKernel3 ]; then
+  git clone -q https://github.com/Kyvangka1610/AnyKernel3.git
+ fi;
+ mv -f $out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+ mv -f $out/arch/arm64/boot/dtbo.img AnyKernel3
+ cd AnyKernel3
+ zip -r9 "$HOME/$ZIPNAME" *
+ cd ..
+ rm -rf AnyKernel3
+ echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+ echo -e "Zip: $ZIPNAME\n"
+ rm -rf $out
+else
+ echo -e "\nCompilation failed!\n"
+fi;
+if [ $R == y ]; then
+ git rs --hard $(git log --pretty=oneline|head -n2|tail -n1|awk '{ print $1}')
+fi;
+exit 0
