7daf34a07b4f615238a506606c2c23eb5baf3de1
kyvangka1610 <kyvangka2002@gmail.com>
2021-01-31 09:37:25 +0700

    SixTeen : build.sh:switch to clang 12.0.2 + GCC 10.2
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- build.sh
+++ build.sh
@@ -6,11 +6,9 @@ PARENT_DIR="$(dirname "$KERNEL_DIR")"
 KERN_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
 export KBUILD_BUILD_USER="elang"
 export KBUILD_BUILD_HOST="kyvangkaelang"
-export PATH="/home/kyvangka1610/toolchain/Clang12/bin:$PATH"
-export LD_LIBRARY_PATH="/home/kyvangka1610/toolchain/Clang12/lib:$LD_LIBRARY_PATH"
-export KBUILD_COMPILER_STRING="$(/home/kyvangka1610/toolchain/Clang12/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
-export CROSS_COMPILE=/home/kyvangka1610/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=/home/kyvangka1610/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export PATH="/home/kyvangka1610/toolchain/clang-r407598b/bin:$PATH"
+export LD_LIBRARY_PATH="/home/kyvangka1610/toolchain/clang-r407598b/lib:$LD_LIBRARY_PATH"
+export KBUILD_COMPILER_STRING="$(/home/kyvangka1610/toolchain/clang-r407598b/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
 export out=/home/kyvangka1610/out-new-Q
 
 # Functions
@@ -20,13 +18,13 @@ clang_build () {
                           CC="clang" \
                           AR="llvm-ar" \
                           NM="llvm-nm" \
-			   LD="ld.lld" \
-			   AS="llvm-as" \
-			   OBJCOPY="llvm-objcopy" \
-			   OBJDUMP="llvm-objdump" \
+						  LD="ld.lld" \
+			              AS="llvm-as" \
+			              OBJCOPY="llvm-objcopy" \
+			              OBJDUMP="llvm-objdump" \
                           CLANG_TRIPLE=aarch64-linux-gnu- \
-                          CROSS_COMPILE=$CROSS_COMPILE \
-                          CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32
+                          CROSS_COMPILE="/home/kyvangka1610/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-" \
+                          CROSS_COMPILE_ARM32="/home/kyvangka1610/toolchain/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-"
 }
 
 # Build kernel
