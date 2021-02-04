c9d50ea76f6dd8fa4c355397303dd2d906840f61
kyvangka1610 <kyvangka2002@gmail.com>
2021-02-04 17:50:17 +0700

    SixTeen : build.sh:switch to proton clang 13 + GCC 10.2
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- build.sh
+++ build.sh
@@ -1,36 +1,53 @@
 echo -e "\nStarting compilation...\n"
+
 # ENV
 CONFIG=vendor/sixteen_defconfig
 KERNEL_DIR=$(pwd)
 PARENT_DIR="$(dirname "$KERNEL_DIR")"
-KERN_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
+KERN_IMG="/home/kyvangka1610/out-new-R/out/arch/arm64/boot/Image.gz-dtb"
 export KBUILD_BUILD_USER="elang"
 export KBUILD_BUILD_HOST="kyvangkaelang"
-export PATH="/home/kyvangka1610/toolchain/Clang12/bin:$PATH"
-export LD_LIBRARY_PATH="/home/kyvangka1610/toolchain/Clang12/lib:$LD_LIBRARY_PATH"
-export KBUILD_COMPILER_STRING="$(/home/kyvangka1610/toolchain/Clang12/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
-export CROSS_COMPILE=/home/kyvangka1610/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=/home/kyvangka1610/toolchain/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export PATH="/home/kyvangka1610/toolchain/proton-clang/bin:$PATH"
+export LD_LIBRARY_PATH="/home/kyvangka1610/toolchain/proton-clang/lib:$LD_LIBRARY_PATH"
+export KBUILD_COMPILER_STRING="$(/home/kyvangka1610/toolchain/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
 export out=/home/kyvangka1610/out-new-R
 
 # Functions
 clang_build () {
-    make -j4 O=$out \
+    make -j$(nproc --all) O=$out \
                           ARCH=arm64 \
                           CC="clang" \
                           AR="llvm-ar" \
                           NM="llvm-nm" \
-			   LD="ld.lld" \
-			   AS="llvm-as" \
-			   OBJCOPY="llvm-objcopy" \
-			   OBJDUMP="llvm-objdump" \
+						  LD="ld.lld" \
+			              AS="llvm-as" \
+			              OBJCOPY="llvm-objcopy" \
+			              OBJDUMP="llvm-objdump" \
                           CLANG_TRIPLE=aarch64-linux-gnu- \
-                          CROSS_COMPILE=$CROSS_COMPILE \
-                          CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32
+                          CROSS_COMPILE="/home/kyvangka1610/toolchain/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-" \
+                          CROSS_COMPILE_ARM32="/home/kyvangka1610/toolchain/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-"
 }
 
 # Build kernel
 make O=$out ARCH=arm64 $CONFIG > /dev/null
 echo -e "${bold}Compiling with CLANG${normal}\n$KBUILD_COMPILER_STRING"
+echo -e "\nCompiling $ZIPNAME\n"
 clang_build
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+if [ -f "$out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "$out/arch/arm64/boot/dtbo.img" ]; then
+ echo -e "\nKernel compiled succesfully! Zipping up...\n"
+ ZIPNAME="SixTeen•Kernel•R•Ginkgo•Willow-$(date '+%Y%m%d-%H%M').zip"
+ if [ ! -d AnyKernel3 ]; then
+  git clone -q https://github.com/Kyvangka1610/AnyKernel3.git
+ fi;
+ mv -f $out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+ mv -f $out/arch/arm64/boot/dtbo.img AnyKernel3
+ cd AnyKernel3
+ zip -r9 "/home/kyvangka1610/$ZIPNAME" *
+ cd ..
+ rm -rf AnyKernel3
+ echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+ echo -e "Zip: $ZIPNAME\n"
+ rm -rf $out
+else
+ echo -e "\nCompilation failed!\n"
+fi;
