e369b66d50e40a170748200143f57a1d40e396b9
kyvangka1610 <kyvangka2002@gmail.com>
2024-02-22 12:22:59 +0700

    build.sh: update
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- build-miui.sh
+++ build-miui.sh
@@ -2,7 +2,7 @@ echo -e "\nStarting compilation...\n"
 
 # Setup and apply patch KernelSU in root dir
 if ! [ -d "$KERNEL_DIR"/KernelSU ]; then
-	curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+	curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s 
 	git apply KernelSU-hook.patch
 else
 		echo -e "Setup KernelSU failed, stopped build now..."
